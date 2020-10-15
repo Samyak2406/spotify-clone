25c0158c93e494d867b83bb853a316004878d9ec
Samyak2406 <samyak.vce18@sot.pdpu.ac.in>
2020-10-15 15:42:56 +0530

    responsive grid

--- lib/constants.dart
+++ lib/constants.dart
@@ -13,5 +13,18 @@ var sidebar =TextStyle(
   color: Colors.grey,
   fontSize: 15,
   fontWeight: FontWeight.w600,
+);
 
+var cardtitle =TextStyle(
+  color: Colors.white,
+  fontSize: 13,
+  letterSpacing: 1.2,
+  fontWeight: FontWeight.w700,
+);
+
+var cardsubtitle =TextStyle(
+  color: Colors.grey,
+  letterSpacing: 1.2,
+  fontSize: 11,
+  fontWeight: FontWeight.w400,
 );
--- lib/homepage.dart
+++ lib/homepage.dart
@@ -3,8 +3,49 @@ import 'constants.dart';
 import 'package:line_icons/line_icons.dart';
 import 'w_homePage.dart';
 
-class homepage extends StatelessWidget {
+class homepage extends StatefulWidget {
   static const id = "homepage";
+
+  @override
+  _homepageState createState() => _homepageState();
+}
+
+
+
+class _homepageState extends State<homepage> {
+
+
+
+
+  int gridcount  = 2;
+
+  int gridCounter(context){
+    var width = MediaQuery.of(context).size.width;
+//    print(width);
+    if(width>=1100){
+      setState(() {
+        gridcount=5;
+      });
+    }
+  else if(width<1100 && width>=800 ){
+    setState(() {
+      gridcount=4;
+    });
+    }
+  else if(width<800 && width>650){
+    setState(() {
+      gridcount=3;
+    });
+    }
+    else{
+      setState(() {
+        gridcount=2;
+      });
+      print(gridcount);
+    }
+    return gridcount;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -99,16 +140,60 @@ class homepage extends StatelessWidget {
                               height: 60,
                               width: double.infinity,
                             ),
+                            SizedBox(height: 15,),
                             GridView.count(
+                              childAspectRatio: 0.7,
                               shrinkWrap: true,
-                              crossAxisCount: 5,
-                              crossAxisSpacing: 10,
-                              mainAxisSpacing: 10,
+                              crossAxisCount: gridCounter(
+                                context
+                              ),
+                              crossAxisSpacing: 20,
+                              mainAxisSpacing: 20,
                               children: List.generate(
-                                40,
-                                (index) => Container(
-                                  color: Colors.teal,
-                                ),
+                                6,
+                                (index) => songcard("images/kabira.jpg","Kabira","Tochi Raina, Rekha Bhardwaj"),
+                              ),
+                            ),
+                            Container(
+                              padding: EdgeInsets.symmetric(vertical: 15),
+                              child: Row(
+                                crossAxisAlignment: CrossAxisAlignment.end,
+                                mainAxisAlignment:
+                                MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(
+                                    "Charts",
+                                    style: TextStyle(
+                                      decoration: TextDecoration.underline,
+                                      color: Colors.white,
+                                      fontSize: 22,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  Text(
+                                    "SEE ALL",
+                                    style: TextStyle(
+                                      letterSpacing: 1.5,
+                                      fontWeight: FontWeight.w700,
+                                      fontSize: 11,
+                                      color: Colors.grey,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              height: 60,
+                              width: double.infinity,
+                            ),
+                            SizedBox(height: 15,),
+                            GridView.count(
+                              childAspectRatio: 0.7,
+                              shrinkWrap: true,
+                              crossAxisCount: gridcount,
+                              crossAxisSpacing: 20,
+                              mainAxisSpacing: 20,
+                              children: List.generate(
+                                6,
+                                    (index) => songcard("images/kabira.jpg","Kabira","Tochi Raina, Rekha Bhardwaj"),
                               ),
                             ),
                           ],
@@ -122,6 +207,54 @@ class homepage extends StatelessWidget {
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class songcard extends StatelessWidget {
+  String imagepath,title,subtitle;
+
+  songcard(
+      this.imagepath,
+      this.title,
+      this.subtitle
+      );
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: FractionallySizedBox(
+          heightFactor: 0.9,
+          widthFactor: 0.8,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Image.asset(imagepath),
+              Flexible(
+                child: Text(
+                  title,
+                  overflow: TextOverflow.ellipsis,
+                  style: cardtitle,
+                ),
+              ),
+              Flexible(
+                child: Text(
+                  subtitle,
+                  overflow: TextOverflow.ellipsis,
+                  style: cardsubtitle,
+                  maxLines: 3,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: Color(0xff171717),
       ),
     );
   }
